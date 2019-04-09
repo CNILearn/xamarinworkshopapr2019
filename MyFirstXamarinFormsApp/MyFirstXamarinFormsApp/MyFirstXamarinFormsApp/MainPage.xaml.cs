8eaa54d765be1834019039bcfcb714755b3ab869
Christian Nagel <christian@christiannagel.com>
2019-04-09 10:21:37 +0200

    xamarin.forms sample

---
+++ MyFirstXamarinFormsApp/MyFirstXamarinFormsApp/MyFirstXamarinFormsApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyFirstXamarinFormsApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            label1.Text = entry1.Text;
+        }
+    }
+}
