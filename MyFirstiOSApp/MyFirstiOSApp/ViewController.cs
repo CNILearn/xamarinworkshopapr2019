c199a808a8d9a51ee88b908852a85c4616dade1a
Christian Nagel <christian@christiannagel.com>
2019-04-09 08:14:05 +0200

    ios sample

---
+++ MyFirstiOSApp/MyFirstiOSApp/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace MyFirstiOSApp
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void Button1_TouchUpInside(UIButton sender)
+        {
+            label1.Text = text1.Text;
+        }
+    }
+}
---
+++ MyFirstiOSApp/MyFirstiOSApp/ViewController.designer.cs
@@ -0,0 +1,60 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace MyFirstiOSApp
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton button1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel label1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton navigateButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField text1 { get; set; }
+
+        [Action ("Button1_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Button1_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (button1 != null) {
+                button1.Dispose ();
+                button1 = null;
+            }
+
+            if (label1 != null) {
+                label1.Dispose ();
+                label1 = null;
+            }
+
+            if (navigateButton != null) {
+                navigateButton.Dispose ();
+                navigateButton = null;
+            }
+
+            if (text1 != null) {
+                text1.Dispose ();
+                text1 = null;
+            }
+        }
+    }
+}
