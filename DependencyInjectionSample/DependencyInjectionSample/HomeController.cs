20ef4717f299afe8c8414197b9d0e7c60b6c8205
Christian Nagel <christian@christiannagel.com>
2019-04-10 12:03:08 +0200

    DI Sample

---
+++ DependencyInjectionSample/DependencyInjectionSample/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependencyInjectionSample
+{
+    public class HomeController
+    {
+        private readonly IGreetingService _greetingService;
+        public HomeController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService ?? throw new ArgumentNullException(nameof(greetingService));
+        }
+
+        public string Hello(string name) => _greetingService.Greet(name);
+
+    }
+}
---
+++ DependencyInjectionSample/DependencyInjectionSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DependencyInjectionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Container = RegisterServices();
+            var controller = Container.GetService<HomeController>();
+            var greet = controller.Hello("Stephanie");
+            Console.WriteLine(greet);
+        }
+
+        public static IServiceProvider Container { get; private set; }
+
+        public static IServiceProvider RegisterServices()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<IGreetingService, GreetingService>();
+            services.AddTransient<HomeController>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
