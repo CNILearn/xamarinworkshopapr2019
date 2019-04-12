5e6e2ccb32154dbf945eb310ff6c7091867a2c3b
Christian Nagel <christian@christiannagel.com>
2019-04-12 10:36:08 +0200

    essentials

---
+++ EssentialsSample1/EssentialsSample1/EssentialsSample1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace EssentialsSample1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnVibrate(object sender, EventArgs e)
+        {
+            Vibration.Vibrate();
+        }
+
+        private async void OnSpeak(object sender, EventArgs e)
+        {
+            await TextToSpeech.SpeakAsync("Hello, World!");
+        }
+    }
+}
